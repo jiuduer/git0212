fc0b36f324c46b5f751e97210e9f81cbb339bea5
jiudu <jiuduer0207@163.com>
2022-06-14 14:58:25 +0800

    first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,4 +1,8 @@
 package com.atguigu.git;
 
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello atguigu");
+        System.out.println("hello master");
+    }
 }
