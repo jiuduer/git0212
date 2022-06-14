9a688280f0e5f6ab814ef333456103e042980b4b
jiudu <jiuduer0207@163.com>
2022-06-14 15:36:17 +0800

    push test commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hahahaha");
         System.out.println("master");
         System.out.println("nihaoya");
+        System.out.println("push test");
     }
 }
